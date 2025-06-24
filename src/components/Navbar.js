be3f12d7bd75775687dee59b2c66b69f89a785c1
AlexGaynetdinov <alex.gaynetdinov@alu.uclm.es>
2025-06-24 03:25:47 +0200

    screen adaptable navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,8 +15,23 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">Pay App</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto">
+
+        {/* Hamburger toggle button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible section */}
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {!token ? (
               <>
                 <li className="nav-item">
@@ -49,17 +64,22 @@ const Navbar = () => {
                 <li className="nav-item">
                   <Link className="nav-link" to="/friends">Friends</Link>
                 </li>
-                {user?.role === 'admin' && (
-                  <li className="nav-item">
-                    <Link className="btn btn-outline-warning ms-2" to="/admin">Admin</Link>
-                  </li>
-                )}
-                <li className="nav-item">
-                  <button className="btn btn-outline-light ms-2" onClick={handleLogout}>Logout</button>
-                </li>
               </>
             )}
           </ul>
+
+          {token && (
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              {user?.role === 'admin' && (
+                <li className="nav-item">
+                  <Link className="btn btn-warning btn-sm me-2" to="/admin">Admin</Link>
+                </li>
+              )}
+              <li className="nav-item">
+                <button className="btn btn-outline-light btn-sm" onClick={handleLogout}>Logout</button>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </nav>
